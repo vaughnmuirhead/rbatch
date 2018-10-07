ea3214512fe4b04a5862da9ba5fd1135594d99f2
Vaughn Muirhead <vaughn.muirhead@capgemini.com>
2018-10-07 16:27:50 +1100

    Updated test.R and config.yml

--- test.R
+++ test.R
@@ -1,33 +1,37 @@
 library(bigrquery)
+library(glue)
 config <- config::get()
 set_service_token(config$credentials)
 
 project <- config$projectid
-dataset <- bq_dataset(config$project, config$dataset)
-table <- bq_table(project, dataset, config$table)
+ds <- bq_dataset(config$project, config$dataset)
+tbl <- bq_table(config$projectid, config$dataset, config$table)
 
-if (!bq_dataset_exists(dataset))
+# Create dataset if it doesn't already exist.
+if (!bq_dataset_exists(ds))
 {
 	msg <- 'Dataset {config$dataset} does not exist. Creating...'
 	print(glue(msg))
-	bq_dataset_create(dataset)
+	bq_dataset_create(ds)
 }
 
-if (!bq_table_exists(table))
+# Create table if it doesn't already exist.
+if (!bq_table_exists(tbl))
 {
 	msg <- 'Table {config$table} does not exist. Creating...'
 	print(glue(msg))
-	bq_table_create(table, fields = bq_field("Name", "string"), ("Counter", "integer"))
+	bq_table_create(tbl, fields = list(bq_field("Name", "string"), bq_field("Counter", "integer")))
 	print('BQ table created.')
 	
-	sql <- "INSERT INTO `{config$dataset}.{config$table}` (Name, Counter) VALUES ('Rjobs', 0)"
-	glue(sql)
+	# Init table with a value to update.
+	sql <- glue("INSERT INTO {config$dataset}.{config$table} (Name, Counter) VALUES ('Rjobs', 0)")
+	
 	data <- bq_project_query(project, sql)
 	print('BQ table initialised successfully.')
 }
  
-sql <- "UPDATE `{config$dataset}.{config$table}` set Counter = Counter + 1 WHERE Name = 'Rjobs'" # Example query.
-glue(sql)
+# This query will simply increment the Counter value by one each time it is executed.
+sql <- glue("UPDATE {config$dataset}.{config$table} set Counter = Counter + 1 WHERE Name = 'Rjobs'") # Example query.
  
 data <- bq_project_query(project, sql)
 print ("BQ query executed successfully!")
