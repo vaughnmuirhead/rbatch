256ead8fec824ab1b818f4ef5f2b36bbf111636a
Vaughn Muirhead <vaughn.muirhead@capgemini.com>
2018-10-07 14:00:28 +1100

    Working on auto config.

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,5 @@ COPY . /usr/local/src/myscripts
 WORKDIR /usr/local/src/myscripts
 RUN R -e "devtools::install_github('r-dbi/bigrquery')"
 RUN R -e "install.packages('config')"
+RUN R -e "install.packages('glue')"
 CMD ["Rscript", "test.R"]
--- test.R
+++ test.R
@@ -3,8 +3,31 @@ config <- config::get()
 set_service_token(config$credentials)
 
 project <- config$projectid
+dataset <- bq_dataset(config$project, config$dataset)
+table <- bq_table(project, dataset, config$table)
+
+if (!bq_dataset_exists(dataset))
+{
+	msg <- 'Dataset {config$dataset} does not exist. Creating...'
+	print(glue(msg))
+	bq_dataset_create(dataset)
+}
+
+if (!bq_table_exists(table))
+{
+	msg <- 'Table {config$table} does not exist. Creating...'
+	print(glue(msg))
+	bq_table_create(table, fields = bq_field("Name", "string"), ("Counter", "integer"))
+	print('BQ table created.')
+	
+	sql <- "INSERT INTO `{config$dataset}.{config$table}` (Name, Counter) VALUES ('Rjobs', 0)"
+	glue(sql)
+	data <- bq_project_query(project, sql)
+	print('BQ table initialised successfully.')
+}
  
-sql <- "UPDATE `test1.test2` set Counter = Counter + 1 WHERE AddressId = 'testaddress'" # Example query.
+sql <- "UPDATE `{config$dataset}.{config$table}` set Counter = Counter + 1 WHERE Name = 'Rjobs'" # Example query.
+glue(sql)
  
 data <- bq_project_query(project, sql)
 print ("BQ query executed successfully!")
